cb67873b45f2eadbffe38551f4041d246c666288
Feavel <tanuki20966@gmail.com>
2022-04-15 23:17:31 +0800

    check

--- pages/Works.js
+++ pages/Works.js
@@ -1,13 +1,22 @@
-import { Box, SimpleGrid } from "@chakra-ui/react";
+import { Box, Container, Heading, SimpleGrid } from "@chakra-ui/react";
 import { WorkGridItem } from "../components/Content/Paragraph/GridItem";
 import Layout from "../components/Layout/Layout";
+import albumPic1 from "../public/images/album/albumPic1_helloKitty.jpg";
 
 export default function Works() {
   return (
     <Layout title="作品">
-      <SimpleGrid columns={[1, 2, 3]} spacing={8}>
-        <Box>Work in Progress</Box>
-      </SimpleGrid>
+      <Container>
+        <Heading as="h3" fontSize={20} mb={4}>
+          Sound Engeneering and Music Production
+        </Heading>
+        <SimpleGrid columns={[1, 2, 4]} spacing={8}>
+          <WorkGridItem id="no1" thumbnail={albumPic1}></WorkGridItem>
+          <WorkGridItem id="no1" thumbnail={albumPic1}></WorkGridItem>
+          <WorkGridItem id="no1" thumbnail={albumPic1}></WorkGridItem>
+          <WorkGridItem id="no1" thumbnail={albumPic1}></WorkGridItem>
+        </SimpleGrid>
+      </Container>
     </Layout>
   );
 }
