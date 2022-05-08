78c47f5f04c7ffb05c019f452189de5028fc5a04
Feavel <tanuki20966@gmail.com>
2022-05-08 17:55:08 +0800

    sese

--- pages/Posts.js
+++ pages/Posts.js
@@ -2,68 +2,52 @@ import Layout from "../components/Layout/Layout";
 import { Box, Button, Heading, SimpleGrid } from "@chakra-ui/react";
 import Parallax from "../components/Parallax";
 import NextLink from "next/link";
-import { WorkGridItem } from "../components/Content/Paragraph/GridItem";
+import {
+  WorkGridItem,
+  GridItem,
+} from "../components/Content/Paragraph/GridItem";
 
-import albumPic2 from "../public/images/album/albumPic4_helloKitty.jpg";
-import albumPic9 from "../public/images/album/albumPic9_helloKitty.jpg";
+import PostPic1 from "../public/images/posts/1-daniil/dan.jpg";
+import PostPic2 from "../public/images/posts/2-cool/IMG_4054.jpg";
+import PostPic3 from "../public/images/posts/3-dark/IMG_4313.jpg";
 
 export default function Posts() {
   return (
     <Layout title="帖子">
       <Heading as="h3" fontSize={30} textAlign="center" mt="45%" mb="45%">
-        <Parallax speed={-1}>我的帖子</Parallax>
+        <Parallax speed={-1}>我的火热🔥帖子</Parallax>
         <Parallax speed={-3}>↓</Parallax>
       </Heading>
-      <SimpleGrid columns={[1, 2, 3]} spacing={8} mt="50%" mb={200}>
+      <SimpleGrid columns={[1, 3, 3]} spacing={8} mt="30%">
         <Box mb={4}>
-          <WorkGridItem id="Cute" thumbnail={albumPic2} title="火辣男朋友">
-            Hot Boy Friend
-          </WorkGridItem>
+          <GridItem
+            href="https://www.douyin.com/video/7073839728801484073?modeFrom=userPost&secUid=MS4wLjABAAAABZed-RkD7nT5SHozVpyjQJmhZFNXQWWXrh6oTn_sX9E"
+            thumbnail={PostPic1}
+            title="#恋爱日常 #cute"
+          >
+            最近是素颜糖🍬💕
+          </GridItem>
         </Box>
         <Box mb={4}>
-          <WorkGridItem id="Cute" thumbnail={albumPic2} title="出去玩啦">
-            Went outside
-          </WorkGridItem>
+          <GridItem
+            href="https://www.douyin.com/video/7092731450738380032?modeFrom=userPost&secUid=MS4wLjABAAAABZed-RkD7nT5SHozVpyjQJmhZFNXQWWXrh6oTn_sX9E"
+            thumbnail={PostPic2}
+            title="#女明星"
+          >
+            #浪漫不死 亞人类大逃杀第三名就是我！
+          </GridItem>
         </Box>
         <Box mb={4}>
-          <WorkGridItem id="Cute" thumbnail={albumPic2} title="可爱">
-            abc
-          </WorkGridItem>
-        </Box>
-      </SimpleGrid>
-
-      <Parallax speed={-2}>
-        <Heading as="h3" variant="section-title" align="center">
-          走秀
-        </Heading>
-      </Parallax>
-
-      <SimpleGrid columns={[1, 2, 2]} spacing={8} mb={200}>
-        <Box mb={4}>
-          <WorkGridItem id="Cute" thumbnail={albumPic2} title="红钻">
-            Red Diamond
-          </WorkGridItem>
-        </Box>
-        <Box mb={4}>
-          <WorkGridItem id="Cute" thumbnail={albumPic2} title="亚文化 · 油">
-            Oil y2k Culture
+          <WorkGridItem
+            href="https://www.douyin.com/video/7007036591206190371?modeFrom=userPost&secUid=MS4wLjABAAAABZed-RkD7nT5SHozVpyjQJmhZFNXQWWXrh6oTn_sX9E"
+            thumbnail={PostPic3}
+            title="#暗黑系"
+          >
+            #哥特 #亚文化 又是营业的一天
           </WorkGridItem>
         </Box>
       </SimpleGrid>
 
-      <Parallax speed={-1.2}>
-        <Heading as="h3" variant="section-title" align="center">
-          与其他作品
-        </Heading>
-      </Parallax>
-
-      <SimpleGrid columns={[1, 1, 1]} spacing={8} mb="10%">
-        <Box mb={4}>
-          <WorkGridItem id="Cute" thumbnail={albumPic2} title="可爱">
-            Cute Make-up
-          </WorkGridItem>
-        </Box>
-      </SimpleGrid>
       <Box align="center" my="10%">
         <NextLink href="/Album" passHref scroll={false}>
           <Button colorScheme="pink">我的相册</Button>
